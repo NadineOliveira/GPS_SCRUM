e3c5a8a7665560d24c456df1b2c8c377fe5c9914
Nadine <nadioliv@gmail.com>
2019-06-08 01:57:28 +0100

    Adciona query que conta tarefas por utilizador

--- GPS_REST/controllers/tarefas.js
+++ GPS_REST/controllers/tarefas.js
@@ -2,16 +2,33 @@ var Sprint = require('./ModelConnections').sprint;
 var Tarefa = require('./ModelConnections').tarefa;
 var db = require('./ModelConnections').db;
 
-module.exports.addTarefa = async function(desc,idSprint){
+module.exports.addTarefa = async function(desc, idSprint) {
     var result;
     await Tarefa.create({
         descricao: desc,
         idSprint: idSprint,
         data: "NAO"
     }).then(s => {
-                result = s;
+        result = s;
     }).catch(err => {
         result = err
     });
     return result;
 }
+
+module.exports.countTarefasUser = async function(idProjeto, username) {
+    var result = 0;
+    await Realiza.count({
+            where: {
+                idProjeto: idProjeto,
+                username: username
+            }
+        })
+        .then(values => {
+            result = values
+        })
+        .catch(err => {
+            result = err;
+        });
+    return result;
+};
